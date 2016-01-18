2e1dfdbd269a14f973ea177dc5b92bc26b298ddd
Dom Batten <dominic.batten@googlemail.com>
2016-01-18 11:14:33 +0000

    test appearing on screen

---
+++ js/components/Piece.react.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Piece = React.createClass({
+  render: function() {
+    return <span>test</span>;
+  }
+});
+
+module.exports = Piece;
--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Piece = require('./components/Piece.react');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Piece />,
+  document.getElementById('root')
+);
