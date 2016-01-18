8d926fd05755b34fc22ba2a6084a96955c5f7340
Dom Batten <dominic.batten@googlemail.com>
2016-01-18 12:08:18 +0000

    board rendering on page

--- js/components/Board.react.js
+++ js/components/Board.react.js
@@ -4,16 +4,44 @@ var Piece  = require('./Piece.react');
 
 var Board = React.createClass({
 
-  render: function() {
+  renderSquare: function(i) {
+    var x = i % 16;
+    var y = Math.floor(i / 16);
+    var pieceX = this.props.piecePosition[0];
+    var pieceY = this.props.piecePosition[1];
+    var piece  = (x === pieceX && y === pieceY) ? <Piece /> : null;
+
     return (
-      <div>
+      <div key={i}
+            style={{ width: '6.25%', height: '6.25%' }}>
         <Square>
-          <Piece />
+          {piece}
         </Square>
       </div>
     );
-  }
+  },
 
+  render: function() {
+    var self = this;
+    var squares = [];
+    (function addSquare(index) {
+      squares.push(self.renderSquare(index));
+      index ++;
+      if(index > 256) { return };
+      addSquare(index);
+    }(0));
+
+    return (
+      <div style={{
+        width:    '100%',
+        height:   '100%',
+        display:  'flex',
+        flexWrap: 'wrap'
+      }}>
+        {squares}
+      </div>
+    );
+  }
 });
 
 module.exports = Board;
