cb93875cf3696cbb91e1d17cebabec736392e8fd
Dom Batten <dominic.batten@googlemail.com>
2016-01-19 18:53:23 +0000

    game spec added

---
+++ __tests__/Game-spec.react.js
@@ -0,0 +1,14 @@
+jest.dontMock('../js/components/Game.react.js');
+
+describe('Game', function() {
+
+  var React = require('react/addons');
+  var TestUtils = React.addons.TestUtils;
+  var Game;
+
+  beforeEach(function() {
+    Game = require('../js/components/Game.react');
+  });
+
+
+});
