8af51159127216628217ef20534f094a676dc2ce
Dom Batten <dominic.batten@googlemail.com>
2016-01-19 10:50:04 +0000

    added black and white checks

--- js/components/Board.react.js
+++ js/components/Board.react.js
@@ -20,10 +20,11 @@ var Board  = React.createClass({
            draggable="true">
       </div> :
       null;
+    var colour = (x + y) % 2 === 1 ? 'black' : 'white';
 
     return (
       <div key={i}
-           style={{ width: '6.25%', height: '6.25%' }}
+           style={{ width: '6.25%', height: '6.25%', backgroundColor: colour }}
            onDragOver={this.dragOver}
            onDrop={this.drop.bind(this, x, y)}>
         <Square>
